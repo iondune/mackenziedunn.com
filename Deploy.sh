305c0e5fda0bee9729f3ee1751e927f5698a722c
Ian Dunn <iondune@gmail.com>
2014-10-30 04:55:54 +0000

    Update deploymet script

--- Deploy.sh
+++ Deploy.sh
@@ -1,9 +1,8 @@
 echo "Pulling changes from repository..."
 echo "----------------------------------"
-hg -R ~/Projects/ionJekyll/ pull -u
+hg -R ~/Projects/MackenzieBeaschler/ pull -u
 echo
 echo "Synchronizing changes with webserver..."
 echo "---------------------------------------"
-cd ~/Projects/ionJekyll/
 jekyll build
-rsync -a _site/ /var/www/iondune.com/public_html/
+rsync -a _site/ /var/www/mackenziebeaschler.com/public_html/
